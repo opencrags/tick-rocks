bc38d7030a60d64b756c8b2d717feac50dfad44d
FelixAbrahamsson <felixab@kth.se>
2022-03-06 14:55:08 +0100

    feature: sortable table with routes

--- src/pages/crag-list.js
+++ src/pages/crag-list.js
@@ -1,18 +1,26 @@
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
 import { Box, Flex, Heading, LinkBox, Text } from '@chakra-ui/layout'
-import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
-import { AgGridColumn, AgGridReact } from 'ag-grid-react'
+import { Link, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { useState } from 'react'
 import { Link as RouterLink, useParams } from 'react-router-dom'
 import { CragBanner, CragBannerMenu } from '../components/crag-banner'
-import { mostVoted, useClimbs, useCrag } from '../utils/backend'
+import {
+  mostVoted,
+  useClimbs,
+  useCrag,
+  useGradeSystemGrades,
+  useSector,
+} from '../utils/backend'
+
+const defaultSortAscending = false
 
 export default function CragList() {
   const { cragId } = useParams()
   const { crag, error: errorCrag } = useCrag(cragId)
   const { climbs, error: errorClimbs } = useClimbs({ crag_id: cragId })
 
-  if (crag === undefined) {
-    return ''
+  if (crag === undefined || climbs === undefined) {
+    return null
   }
 
   if (errorCrag || errorClimbs) {
@@ -53,31 +61,166 @@ export default function CragList() {
             className="ag-theme-alpine"
             style={{ height: 400, width: '100%' }}
           >
-            <AgGridReact rowData={climbs}>
-              <AgGridColumn
-                field="name_votes"
-                sortable={true}
-                filter={true}
-              ></AgGridColumn>
-              <AgGridColumn
-                field="most_voted_grade"
-                sortable={true}
-                filter={true}
-              ></AgGridColumn>
-              <AgGridColumn
-                field="average_rating"
-                sortable={true}
-                filter={true}
-              ></AgGridColumn>
-              <AgGridColumn
-                field="sector_id"
-                sortable={true}
-                filter={true}
-              ></AgGridColumn>
-            </AgGridReact>
+            <RouteTable climbs={climbs} />
           </div>
         </Flex>
       </Box>
     </Box>
   )
+}
+
+const RouteTable = ({ climbs }) => {
+  const [sortKey, setSortKey] = useState('rating')
+  const [sortAscending, setSortAscending] = useState(defaultSortAscending)
+
+  const { gradeSystemGrades, error } = useGradeSystemGrades()
+  if (gradeSystemGrades === undefined || error) {
+    return null // todo: return a loader
+  }
+  const gradeMap = Object.assign(
+    {},
+    ...gradeSystemGrades.map((grade) => ({
+      [grade.id]: {
+        fuzzyUnifiedRank: grade.fuzzy_unified_rank,
+        grade: grade.grade,
+        rank: grade.rank,
+        system: grade.system,
+      },
+    }))
+  )
+
+  return (
+    <Table size="md">
+      <Thead>
+        <Tr>
+          <SortableColumn
+            name={'Name'}
+            sortKey={'name'}
+            currentSortKey={sortKey}
+            setSortKey={setSortKey}
+            currentSortAscending={sortAscending}
+            setSortAscending={setSortAscending}
+          />
+          <SortableColumn
+            name={'Grade'}
+            sortKey={'grade'}
+            currentSortKey={sortKey}
+            setSortKey={setSortKey}
+            currentSortAscending={sortAscending}
+            setSortAscending={setSortAscending}
+          />
+          <SortableColumn
+            name={'Rating'}
+            sortKey={'rating'}
+            currentSortKey={sortKey}
+            setSortKey={setSortKey}
+            currentSortAscending={sortAscending}
+            setSortAscending={setSortAscending}
+          />
+          <Th>Sector</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {climbs
+          .sort((climbA, climbB) =>
+            sortField(climbA, sortKey, gradeMap) <
+            sortField(climbB, sortKey, gradeMap)
+              ? sortAscending
+                ? -1
+                : 1
+              : sortField(climbB, sortKey, gradeMap) <
+                sortField(climbA, sortKey, gradeMap)
+              ? sortAscending
+                ? 1
+                : -1
+              : 0
+          )
+          .map((climb) => (
+            <Tr key={climb.id}>
+              <NameCell climb={climb} />
+              <Td>{gradeMap[climb.most_voted_grade]?.grade}</Td>
+              <Td>{mostVoted(climb.rating_votes)}</Td>
+              <SectorCell climb={climb} />
+            </Tr>
+          ))}
+      </Tbody>
+    </Table>
+  )
+}
+
+const SortableColumn = ({
+  name,
+  sortKey,
+  currentSortKey,
+  setSortKey,
+  currentSortAscending,
+  setSortAscending,
+}) => {
+  return (
+    <Th
+      onClick={() => {
+        if (currentSortKey === sortKey) {
+          if (currentSortAscending !== defaultSortAscending) {
+            setSortKey(null)
+            setSortAscending(defaultSortAscending)
+          } else {
+            setSortAscending(!defaultSortAscending)
+          }
+        } else {
+          setSortKey(sortKey)
+          setSortAscending(defaultSortAscending)
+        }
+      }}
+    >
+      {name}&nbsp;
+      {sortKey === currentSortKey ? (
+        currentSortAscending ? (
+          <TriangleUpIcon aria-label="sorted descending" />
+        ) : (
+          <TriangleDownIcon aria-label="sorted ascending" />
+        )
+      ) : (
+        ''
+      )}
+    </Th>
+  )
+}
+
+const sortField = (climb, sortKey, gradeMap) => {
+  if (sortKey === 'name') {
+    return mostVoted(climb.name_votes)
+  }
+  if (sortKey === 'grade') {
+    return gradeMap[climb.most_voted_grade].fuzzyUnifiedRank
+  }
+  if (sortKey === 'rating') {
+    return mostVoted(climb.rating_votes)
+  }
+}
+
+const NameCell = ({ climb }) => {
+  const { sector, error } = useSector(climb.sector_id)
+  if (sector === undefined || error) {
+    return null
+  }
+  return (
+    <Td>
+      <Link
+        fontWeight="600"
+        color="blue.400"
+        href={`/crags/${climb.crag_id}/sectors/${sector.id}/climbs/${climb.id}`}
+      >
+        {mostVoted(climb.name_votes)}
+      </Link>{' '}
+    </Td>
+  )
+}
+
+const SectorCell = ({ climb }) => {
+  const { sector, error } = useSector(climb.sector_id)
+  if (sector === undefined || error) {
+    return null
+  }
+  const sectorName = mostVoted(sector.name_votes)
+  return <Td>{sectorName}</Td>
 }
